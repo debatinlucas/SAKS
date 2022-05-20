f6ca3405726cc9212f0f23b658d8d3b8f3c49f08
Lucas Debatin <lucasdebatin@univali.br>
2022-05-20 16:42:54 -0300

    CM - removido importações.

--- biblioteca-services/item-emprestimo-service/src/main/java/br/com/saks/itememprestimoservice/model/ItemEmprestimo.java
+++ biblioteca-services/item-emprestimo-service/src/main/java/br/com/saks/itememprestimoservice/model/ItemEmprestimo.java
@@ -1,11 +1,7 @@
 package br.com.saks.itememprestimoservice.model;
 
-import java.io.Serializable;
-import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.Data;
 
 @Data
